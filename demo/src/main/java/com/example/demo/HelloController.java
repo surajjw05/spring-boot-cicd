03918a02c1b69baa8c45b1d571d6e59d8adb6d4b
Suraj Walve <surajwalve39@gmail.com>
2025-08-01 10:06:15 +0530

    Update HelloController.java

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Spring Boot!";
+        return "Hello from  Boot!";
     }
 
     @GetMapping("/status")
